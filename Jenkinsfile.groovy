4e67e0e3246e2a52d57e6ae2dd55b8d7dae70d86
Gygamyt <gygamyt@gmail.com>
2022-10-28 15:35:20 +0300

    .gdsl added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent {
+        any
+    }
+    tool {
+        gradle('gradle')
+        jdk('jdk')
+    }
+    stages {
+        stage('Git Push') {
+            steps {
+                git credentialsId: 'git_ssh', url: 'github.com/Gygamyt/innowiseTask4Trainee'
+            }
+        }
+        stage('Tests') {
+            steps {
+                sh "gradle cleanTest test"
+            }
+        }
+    }
+}
